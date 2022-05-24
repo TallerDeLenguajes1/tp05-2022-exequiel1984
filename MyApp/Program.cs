fa2e4031d4594ac641825577134794b26c4873d4
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-05-24 12:57:52 -0300

    numero invertido corregido

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hola Mundo");
+Console.WriteLine("Ingrese el numero a invertir ");
+string numero = Console.ReadLine();
+int aux = Convert.ToInt32(numero);
+
+int numeroInvertido = 0;
+
+while (aux > 0){
+    numeroInvertido = numeroInvertido*10 + aux%10;
+    aux /= 10;   
+}
+
+Console.WriteLine("El numero invertido es "+ numeroInvertido);
